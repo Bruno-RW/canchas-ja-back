ab0dba13aff733c1de0b261d66d62647857b5f3e
Bruno Wunsch <bruno_wunsch@hotmail.com>
2025-11-04 20:28:43 -0300

    Added more mock users

--- src/data/mockUsers.py
+++ src/data/mockUsers.py
@@ -11,4 +11,28 @@ mockUsers = [
         "favorites": [1, 2],  # List of favorite product IDs
         "password": "senha12",  # Mock password
     },
+    {
+        "id": 2,
+        "name": "Guilherme Boiaski",
+        "email": "guilherme.boiaski@sou.unijui.edu.br",
+        "type": "C",  # C = Client | H = Host
+        "avatar": "/placeholder.svg?height=40&width=40",
+        "initials": "GB",
+        "isLogin": True,
+        "location": "Horizontina, RS",
+        "favorites": [3, 4],  # List of favorite product IDs
+        "password": "senha12",  # Mock password
+    },
+        {
+        "id": 3,
+        "name": "Matheus Sanaiotto",
+        "email": "matheus.sanaiotto@sou.unijui.edu.br",
+        "type": "C",  # C = Client | H = Host
+        "avatar": "/placeholder.svg?height=40&width=40",
+        "initials": "MS",
+        "isLogin": True,
+        "location": "Santa Rosa, RS",
+        "favorites": [5, 6],  # List of favorite product IDs
+        "password": "senha12",  # Mock password
+    },
 ]
